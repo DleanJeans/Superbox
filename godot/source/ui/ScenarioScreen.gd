82393986fd54f41e9a383cd0cbb9bdc54e4540a7
Dlean Jeans <dleanjeans@gmail.com>
2018-07-05 20:29:19 +0700

    Make ScenarioScreen skippable

--- godot/source/ui/ScenarioScreen.gd
+++ godot/source/ui/ScenarioScreen.gd
@@ -1,6 +1,6 @@
 extends Control
 
-export(float) var showing_duration = 1.5
+export(float) var wait_duration = 1.5
 
 signal scrolled_down
 signal scrolled_up
@@ -8,20 +8,15 @@ signal scrolled_up
 var skip_scroll_down_once = true
 
 func setup(number, name):
-	$Panel/Scenario.text = "Scenerio %s" % number
-	$Panel/Name.text = name
+	$Scenario.text = "Scenerio %s" % number
+	$Name.text = name
 
 func scroll():
 	_scroll_down()
 	_skip_scroll_down_if_requested()
 	
-	yield(get_tree().create_timer(showing_duration), "timeout")
-	_scroll_up()
-
-func _skip_scroll_down_if_requested():
-	if skip_scroll_down_once:
-		$AnimationPlayer.advance(0.5)
-		skip_scroll_down_once = false
+	$Timer.wait_time = wait_duration
+	$Timer.start()
 
 func _scroll_down():
 	$AnimationPlayer.play("ScrollDown")
@@ -29,8 +24,20 @@ func _scroll_down():
 	yield($AnimationPlayer, "animation_finished")
 	emit_signal("scrolled_down")
 
+func _skip_scroll_down_if_requested():
+	if skip_scroll_down_once:
+		$AnimationPlayer.advance(0.5)
+		skip_scroll_down_once = false
+
 func _scroll_up():
 	$AnimationPlayer.play_backwards("ScrollDown")
 	yield($AnimationPlayer, "animation_finished")
 	get_tree().paused = false
 	emit_signal("scrolled_up")
+
+func _process(delta):
+	if not $Timer.is_stopped() \
+	and (Input.is_action_just_pressed("left_click")
+	or Input.is_action_just_pressed("action")):
+		$Timer.stop()
+		_scroll_up()
