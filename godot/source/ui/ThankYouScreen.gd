1eceb4de2fd74ec783059fad1155798dbcfe5785
Dlean Jeans <dleanjeans@gmail.com>
2018-07-12 19:43:45 +0700

    Show attempts in ThankYouScreen

--- godot/source/ui/ThankYouScreen.gd
+++ godot/source/ui/ThankYouScreen.gd
@@ -3,3 +3,9 @@ extends Control
 func scroll_in():
 	Shortcuts.transition_sound.play()
 	$AnimationPlayer.play("ScrollIn")
+	
+	$Attempts.text = "Attempts: %s" % GameData.attempts
+
+func _on_MenuButton_pressed():
+	GameData.reset()
+	get_tree().change_scene("res://source/TitleScreen.tscn")
