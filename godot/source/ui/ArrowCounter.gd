8f8aaf60486f830b9752916a28a48a83201d1a03
Dlean Jeans <dleanjeans@gmail.com>
2018-07-14 23:02:46 +0700

    Fix arrow counter position

--- godot/source/ui/ArrowCounter.gd
+++ godot/source/ui/ArrowCounter.gd
@@ -4,7 +4,7 @@ func update():
 	var arrow_shooter = Shortcuts.arrow_shooter
 	
 	text = str(arrow_shooter.arrows_left)
-	rect_position = arrow_shooter.position
+	rect_global_position = arrow_shooter.position - Vector2(20, 10)
 
 func show():
 	update()
